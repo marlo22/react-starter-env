e2cb3e9d363aba2e84fc9efe6f85b594e7e14966
Marcin Lasecki <marcin.1993@poczta.fm>
2019-03-13 19:08:42 +0100

    Updated to Babel 7, added support for object rest spread and class properties

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const Greeting = () => <h1>Hello world!</h1>;
+class Greeting extends React.Component {
+  state = {
+    initialized: false,
+  }
+
+  componentDidMount() {
+    this.welcome();
+  }
+
+  welcome = () => {
+    this.setState({ initialized: true });
+    console.log('Hello console!');
+  }
+
+  render() {
+    return (
+      <h1>Is app initialized? {this.state.initialized.toString()}</h1>
+    );
+  }
+}
 
 export default Greeting;
